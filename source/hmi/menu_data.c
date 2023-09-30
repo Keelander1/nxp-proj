131282d769795f0d139e13d22d1f3c5820f48545
T-SC <s-tschue@haw-landshut.de>
2023-09-30 21:41:19 +0200

    Update menu_data.c

--- source/hmi/menu_data.c
+++ source/hmi/menu_data.c
@@ -217,7 +217,7 @@ const static menu_entry_t menu_hardware_camera_entries[] = {
 				.en = true,														//element is changeable
 		},
 
-		{		.str = "Pixel_Display",							// new Martin Fürstberger 27.05.23
+		{		.str = "Pixel Display",							// new Martin Fürstberger 27.05.23
 				.type = MENU_PAGE,
 				.func = menu_page_pixel_display_camera,
 				.en = true,
